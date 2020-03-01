57e1ca3d53814e72ab1a2006cc9a5ca379df14bf
PSPshnik <61647101+PSPshnik@users.noreply.github.com>
2020-03-01 13:17:28 +0500

    Update afk.py

--- afk.py
+++ afk.py
@@ -1,3 +1,20 @@
+# -*- coding: future_fstrings -*-
+
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from .. import loader, utils
 
@@ -44,7 +61,7 @@ class AFKMod(loader.Module):
         self._db.set(__name__, "afk", False)
         self._db.set(__name__, "gone", None)
         self._db.set(__name__, "ratelimit", [])
-        await self.allmodules.log("unafk")
+        await self.allmodules.log("unafkk")
         await utils.answer(message, self.strings["back"])
 
     async def watcher(self, message):
@@ -72,4 +89,4 @@ class AFKMod(loader.Module):
             await utils.answer(message, ret)
 
     def get_afk(self):
-        return self._db.get(__name__, "afkk", False)
+        return self._db.get(__name__, "afk", False)
